75d367fa092510bbc1fc09f776e12fe01220119c
brian-unb <brian@uniba.jp>
2025-12-17 10:53:24 +0900

    test

--- playwright.config.js
+++ playwright.config.js
@@ -9,7 +9,10 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+    ['html', { open: 'never' }],
+    ['json', { outputFile: 'playwright-report/results.json' }]
+  ],
 
   projects: [
     {
